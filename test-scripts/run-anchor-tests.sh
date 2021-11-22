df69e971d06add18977d15a970fddb39842d8d6c
0xbigz <0xbigz>
2021-11-22 13:04:59 -0500

    protocol: revert test-scripts to all tests

--- test-scripts/run-anchor-tests.sh
+++ test-scripts/run-anchor-tests.sh
@@ -5,8 +5,6 @@ if [ "$1" != "--skip-build" ]
 fi
 
 test_files=(clearingHouse.ts pyth.ts userAccount.ts admin.ts updateK.ts adminWithdraw.ts curve.ts whitelist.ts fees.ts idempotentCurve.ts maxDeposit.ts deleteUser.ts maxPositions.ts maxReserves.ts roundInFavor.ts cappedSymFunding.ts)
-test_files=(curve.ts)
-# whitelist.ts fees.ts idempotentCurve.ts maxDeposit.ts deleteUser.ts maxPositions.ts maxReserves.ts roundInFavor.ts cappedSymFunding.ts)
 
 for test_file in ${test_files[@]}; do
   export ANCHOR_TEST_FILE=${test_file} && anchor test --skip-build || exit 1;
