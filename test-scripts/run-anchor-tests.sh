50f797ee208b15b675fff733d61956b908c70851
Chris Heaney <chrisheaney30@gmail.com>
2021-12-22 10:44:14 -0600

    remove open_position_with_base_asset_amount

--- test-scripts/run-anchor-tests.sh
+++ test-scripts/run-anchor-tests.sh
@@ -4,7 +4,6 @@ if [ "$1" != "--skip-build" ]
     cp target/idl/clearing_house.json sdk/src/idl/
 fi
 
-#test_files=(openWithBaseAsset.ts roundInFavorBaseAsset.ts order.ts clearingHouse.ts pyth.ts userAccount.ts admin.ts updateK.ts adminWithdraw.ts curve.ts whitelist.ts fees.ts idempotentCurve.ts maxDeposit.ts deleteUser.ts maxPositions.ts maxReserves.ts roundInFavor.ts cappedSymFunding.ts)
 test_files=(order.ts clearingHouse.ts pyth.ts userAccount.ts admin.ts updateK.ts adminWithdraw.ts curve.ts whitelist.ts fees.ts idempotentCurve.ts maxDeposit.ts deleteUser.ts maxPositions.ts maxReserves.ts roundInFavor.ts cappedSymFunding.ts)
 
 for test_file in ${test_files[@]}; do
