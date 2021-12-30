3ca3de189ec47dec8bb0dd1843b179c8869f6bf9
Chris Heaney <chrisheaney30@gmail.com>
2021-12-29 22:06:30 -0600

    sdk: update devnet program id

--- sdk/src/config.ts
+++ sdk/src/config.ts
@@ -11,7 +11,7 @@ export const configs: { [key in DriftEnv]: DriftConfig } = {
 	devnet: {
 		ENV: 'devnet',
 		PYTH_ORACLE_MAPPING_ADDRESS: 'BmA9Z6FjioHJPpjT39QazZyhDRUdZy2ezwx4GiDdE2u2',
-		CLEARING_HOUSE_PROGRAM_ID: 'FVYy6sjW13jzMNJ2abAdsN1iBEDU5cHrZVmiWVPdSA33',
+		CLEARING_HOUSE_PROGRAM_ID: 'EidrjDVk4L3tPZN6Wuxe3iuMN6D3SFZWZnJg8wCoKu6V',
 		USDC_MINT_ADDRESS: '8zGuJQqwhZafTah7Uc7Z4tXRnguqkn5KLFAP8oV6PHe2',
 	},
 	'mainnet-beta': {
