be05f54ad18c48631ea7bd1c3c75505a9a3fdc58
unknown <lmvanderzande@gmail.com>
2022-02-15 11:10:35 -0600

    fix typescript config errors

--- sdk/src/math/fees.ts
+++ sdk/src/math/fees.ts
@@ -4,12 +4,12 @@ import { OrderDiscountTier, OrderFillerRewardStructure } from "../types";
 
 
 export function calculateFeeForMarketOrder(quoteAssetAmount: BN, feeStructure: FeeStructure, discountToken?: AccountInfo, referrer?: ClearingHouseUser ) : Array<BN> {
-    const fee = quoteAssetAmount.mul(feeStructure.feeNumerator).div(feeStructure.feeDenominator)
-    const tokenDiscount = calculateTokenDiscount(fee, feeStructure, discountToken)
-    const [referrerReward, refereeDiscount] = calculateReferralRewardAndRefereeDiscount(fee, feeStructure, referrer)
+    const fee = quoteAssetAmount.mul(feeStructure.feeNumerator).div(feeStructure.feeDenominator);
+    const tokenDiscount = calculateTokenDiscount(fee, feeStructure, discountToken);
+    const [referrerReward, refereeDiscount] = calculateReferralRewardAndRefereeDiscount(fee, feeStructure, referrer);
     const userFee = fee.sub(tokenDiscount).sub(refereeDiscount);
     const feeToMarket = userFee.sub(referrerReward);
-    return [userFee,feeToMarket,tokenDiscount,referrerReward,refereeDiscount]
+    return [userFee,feeToMarket,tokenDiscount,referrerReward,refereeDiscount];
 }
 
 export function calculateTokenDiscount(fee: BN, feeStructure: FeeStructure, discountToken?: AccountInfo) : BN {
@@ -20,11 +20,11 @@ export function calculateTokenDiscount(fee: BN, feeStructure: FeeStructure, disc
     }
 
     Object.keys(feeStructure.discountTokenTiers).forEach(tier => {
-        let possibleDiscount = tryToCalculateTokenDiscountForTier(fee, feeStructure.discountTokenTiers[tier], discountToken)
+        const possibleDiscount = tryToCalculateTokenDiscountForTier(fee, feeStructure.discountTokenTiers[tier], discountToken);
         if (possibleDiscount > discount) {
             discount = possibleDiscount;
         }
-    })
+    });
     
     return discount;
 
@@ -38,7 +38,7 @@ export function tryToCalculateTokenDiscountForTier(fee : BN, tier : { minimumBal
 }
 
 export function calculateTokenDiscountForTier(fee : BN, tier : { minimumBalance: BN, discountNumerator: BN, discountDenominator: BN }) : BN {
-    return fee.mul(tier.discountNumerator).div(tier.discountDenominator)
+    return fee.mul(tier.discountNumerator).div(tier.discountDenominator);
 }
 
 export function belongsToTier(tier: { minimumBalance: BN, discountNumerator: BN, discountDenominator: BN }, discountToken: AccountInfo) : boolean {
@@ -64,36 +64,36 @@ export function calculateOrderFeeTier(feeStructure: FeeStructure, discountToken?
                 tierIndex = index;
                 tier = OrderDiscountTier[discountTier.split('Tier')[0].toUpperCase()];
             }
-        })
+        });
     }
     return tier;
 
 }
 
-export function calculateFeeForLimitOrder(quoteAssetAmount : BN, feeStructure: FeeStructure, fillerRewardStructure: OrderFillerRewardStructure, orderFeeTier: OrderDiscountTier, orderTs: BN, now: BN, referrer?: ClearingHouseUser, fillerIsTaker: boolean = false) : Array<BN> {
+export function calculateFeeForLimitOrder(quoteAssetAmount : BN, feeStructure: FeeStructure, fillerRewardStructure: OrderFillerRewardStructure, orderFeeTier: OrderDiscountTier, orderTs: BN, now: BN, referrer?: ClearingHouseUser, fillerIsTaker = false) : Array<BN> {
     const fee = quoteAssetAmount.mul(feeStructure.feeNumerator).div(feeStructure.feeDenominator);
     const tokenDiscount = calculateTokenDiscountForLimitOrder(fee, feeStructure, orderFeeTier);
     const [referrerReward, refereeDiscount] = calculateReferralRewardAndRefereeDiscount(fee, feeStructure, referrer);
     const userFee = fee.sub(refereeDiscount).sub(tokenDiscount);
     const fillerReward = fillerIsTaker ? ZERO : calculateFillerReward(userFee, orderTs, now, fillerRewardStructure);
     const feeToMarket = userFee.sub(fillerReward).sub(referrerReward);
-    return [userFee, feeToMarket, tokenDiscount, fillerReward, referrerReward, refereeDiscount]
+    return [userFee, feeToMarket, tokenDiscount, fillerReward, referrerReward, refereeDiscount];
 }
 
 export function calculateTokenDiscountForLimitOrder(fee: BN, feeStructure: FeeStructure, orderFeeTier: OrderDiscountTier) : BN {
     let tokenDiscount = ZERO;
     switch(orderFeeTier) {
         case OrderDiscountTier.FIRST:
-            tokenDiscount = calculateTokenDiscountForTier(fee, feeStructure.discountTokenTiers.firstTier)
+            tokenDiscount = calculateTokenDiscountForTier(fee, feeStructure.discountTokenTiers.firstTier);
             break;
         case OrderDiscountTier.SECOND:
-            tokenDiscount = calculateTokenDiscountForTier(fee, feeStructure.discountTokenTiers.secondTier)
+            tokenDiscount = calculateTokenDiscountForTier(fee, feeStructure.discountTokenTiers.secondTier);
             break;
         case OrderDiscountTier.THIRD:
-            tokenDiscount = calculateTokenDiscountForTier(fee, feeStructure.discountTokenTiers.thirdTier)
+            tokenDiscount = calculateTokenDiscountForTier(fee, feeStructure.discountTokenTiers.thirdTier);
             break;
         case OrderDiscountTier.FOURTH:
-            tokenDiscount = calculateTokenDiscountForTier(fee, feeStructure.discountTokenTiers.fourthTier)
+            tokenDiscount = calculateTokenDiscountForTier(fee, feeStructure.discountTokenTiers.fourthTier);
             break;
     }
     return tokenDiscount;
@@ -102,10 +102,10 @@ export function calculateTokenDiscountForLimitOrder(fee: BN, feeStructure: FeeSt
 // https://github.com/indutny/bn.js/pull/277
 // https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method
 function sqrt (input: BN) {
-    var z = new BN(0);
+    let z = new BN(0);
     if (input.gt(new BN(3))) {
       z = input;
-      var x = input.div(new BN(2)).add(new BN(1));
+      let x = input.div(new BN(2)).add(new BN(1));
       while (x.lt(z)) {
         z = x;
         x = input.div(x).add(x).div(new BN(2));
@@ -114,14 +114,14 @@ function sqrt (input: BN) {
       z = new BN(1);
     }
     return z;
-};
+}
 
 export function calculateFillerReward(userFee: BN, orderTs: BN, now: BN, fillerRewardStructure: OrderFillerRewardStructure) : BN {
     // incentivize keepers to prioritize filling older orders (rather than just largest orders)
     // for sufficiently small-sized order, reward based on fraction of fee paid
     const sizeFillerReward = userFee.mul(fillerRewardStructure.rewardNumerator).div(fillerRewardStructure.rewardDenominator);
     const timeSinceOrder = BN.max(new BN(1), now.sub(orderTs));
-	const timeFillerReward = (sqrt(sqrt(timeSinceOrder.mul(new BN(10 ** 8)))).mul(fillerRewardStructure.timeBasedRewardLowerbound)).div(new BN(100))
+	const timeFillerReward = (sqrt(sqrt(timeSinceOrder.mul(new BN(10 ** 8)))).mul(fillerRewardStructure.timeBasedRewardLowerbound)).div(new BN(100));
 
 	return BN.min(sizeFillerReward, timeFillerReward);
 }
