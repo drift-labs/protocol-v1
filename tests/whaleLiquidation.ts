92b326bbadd5ab85824287b29d27c4d0a708eb51
Chris Heaney <chrisheaney30@gmail.com>
2022-02-28 18:16:44 -0600

    new whaletLiquidation test

---
+++ tests/whaleLiquidation.ts
@@ -0,0 +1,143 @@
+import * as anchor from '@project-serum/anchor';
+import { assert } from 'chai';
+import { BN } from '../sdk';
+
+import { Program } from '@project-serum/anchor';
+
+import { PublicKey } from '@solana/web3.js';
+
+import {
+	Admin,
+	findComputeUnitConsumption,
+	MARK_PRICE_PRECISION,
+	PositionDirection,
+} from '../sdk/src';
+
+import {
+	mockOracle,
+	mockUSDCMint,
+	mockUserUSDCAccount,
+	setFeedPrice,
+} from './testHelpers';
+
+describe('whale liquidation', () => {
+	const provider = anchor.Provider.local(undefined, {
+		preflightCommitment: 'confirmed',
+		commitment: 'confirmed',
+	});
+	const connection = provider.connection;
+	anchor.setProvider(provider);
+	const chProgram = anchor.workspace.ClearingHouse as Program;
+
+	let clearingHouse: Admin;
+
+	let userAccountPublicKey: PublicKey;
+
+	let usdcMint;
+	let userUSDCAccount;
+
+	// ammInvariant == k == x * y
+	const mantissaSqrtScale = new BN(Math.sqrt(MARK_PRICE_PRECISION.toNumber()));
+	const ammInitialQuoteAssetReserve = new anchor.BN(5 * 10 ** 13).mul(
+		mantissaSqrtScale
+	);
+	const ammInitialBaseAssetReserve = new anchor.BN(5 * 10 ** 13).mul(
+		mantissaSqrtScale
+	);
+
+	const usdcAmount = new BN(25 * 10 ** 8);
+
+	const maxPositions = 5;
+
+	before(async () => {
+		usdcMint = await mockUSDCMint(provider);
+		userUSDCAccount = await mockUserUSDCAccount(usdcMint, usdcAmount, provider);
+
+		clearingHouse = Admin.from(
+			connection,
+			provider.wallet,
+			chProgram.programId,
+			{
+				commitment: 'confirmed',
+			}
+		);
+		await clearingHouse.initialize(usdcMint.publicKey, true);
+		await clearingHouse.subscribeToAll();
+
+		for (let i = 0; i < maxPositions; i++) {
+			const oracle = await mockOracle(1);
+			const periodicity = new BN(0);
+
+			await clearingHouse.initializeMarket(
+				new BN(i),
+				oracle,
+				ammInitialBaseAssetReserve,
+				ammInitialQuoteAssetReserve,
+				periodicity
+			);
+		}
+
+		[, userAccountPublicKey] =
+			await clearingHouse.initializeUserAccountAndDepositCollateral(
+				usdcAmount,
+				userUSDCAccount.publicKey
+			);
+
+		const usdcPerPosition = usdcAmount
+			.mul(new BN(5))
+			.div(new BN(maxPositions))
+			.mul(new BN(99))
+			.div(new BN(100));
+
+		for (let i = 0; i < maxPositions; i++) {
+			await clearingHouse.openPosition(
+				PositionDirection.LONG,
+				usdcPerPosition,
+				new BN(i),
+				new BN(0)
+			);
+		}
+	});
+
+	after(async () => {
+		await clearingHouse.unsubscribe();
+	});
+
+	it('liquidate', async () => {
+		const markets = clearingHouse.getMarketsAccount();
+		for (let i = 0; i < maxPositions; i++) {
+			const oracle = markets.markets[i].amm.oracle;
+			await setFeedPrice(anchor.workspace.Pyth, 0.85, oracle);
+			await clearingHouse.moveAmmPrice(
+				ammInitialBaseAssetReserve.div(new BN(5)),
+				ammInitialQuoteAssetReserve.div(new BN(6)),
+				new BN(i)
+			);
+		}
+
+		const txSig = await clearingHouse.liquidate(userAccountPublicKey);
+		const computeUnits = await findComputeUnitConsumption(
+			clearingHouse.program.programId,
+			connection,
+			txSig,
+			'confirmed'
+		);
+		console.log('compute units', computeUnits);
+		console.log(
+			'tx logs',
+			(await connection.getTransaction(txSig, { commitment: 'confirmed' })).meta
+				.logMessages
+		);
+
+		const liquidationHistory = clearingHouse.getLiquidationHistoryAccount();
+		const liquidationRecord = liquidationHistory.liquidationRecords[0];
+
+		assert(!liquidationRecord.partial);
+		// 41% of position closed
+		assert(liquidationRecord.baseAssetValue.eq(new BN(10015052925)));
+		assert(liquidationRecord.baseAssetValueClosed.eq(new BN(4172938715)));
+		// 41% of total collateral taken
+		assert(liquidationRecord.totalCollateral.eq(new BN(125131870)));
+		assert(liquidationRecord.liquidationFee.eq(new BN(52138275)));
+	});
+});
