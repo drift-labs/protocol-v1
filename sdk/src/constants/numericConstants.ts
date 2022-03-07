8a711a63145b570c500cb63b7dd9c2f6bd7d0e31
Chris Heaney <chrisheaney30@gmail.com>
2022-03-07 17:23:33 -0500

    sdk: remove liquidationPriceOld

--- sdk/src/constants/numericConstants.ts
+++ sdk/src/constants/numericConstants.ts
@@ -7,8 +7,6 @@ export const TEN_THOUSAND = new BN(10000);
 export const BN_MAX = new BN(Number.MAX_SAFE_INTEGER);
 
 export const MAX_LEVERAGE = new BN(5);
-export const FULL_LIQUIDATION_RATIO = new BN(500);
-export const PARTIAL_LIQUIDATION_RATIO = new BN(625);
 
 export const QUOTE_PRECISION = new BN(10 ** 6);
 export const MARK_PRICE_PRECISION = new BN(10 ** 10);
