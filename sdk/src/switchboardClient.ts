513cd3e8e589fc8a3bb14db6b8553a5f2090a0c6
Chris Heaney <chrisheaney30@gmail.com>
2022-03-09 16:55:00 -0500

    simplify switchboardClient

--- sdk/src/switchboardClient.ts
+++ sdk/src/switchboardClient.ts
@@ -1,38 +1,34 @@
 import {
 	AggregatorAccount,
-	SBV2_DEVNET_PID,
-	SBV2_MAINNET_PID,
+	loadSwitchboardProgram,
+	SwitchboardDecimal,
 } from '@switchboard-xyz/switchboard-v2';
-import { ConfirmOptions, Connection, PublicKey } from '@solana/web3.js';
-import { Wallet } from './wallet';
-import { Program, Provider } from '@project-serum/anchor-v0.22';
+import { Connection, PublicKey } from '@solana/web3.js';
 import { DriftEnv } from './config';
+import { BN } from '@project-serum/anchor';
+import { MARK_PRICE_PRECISION, TEN } from './constants/numericConstants';
+
+type Program = ReturnType<typeof loadSwitchboardProgram>;
 
 export class SwitchboardClient {
-	private programId: PublicKey;
-	private connection: Connection;
-	private wallet: Wallet;
-	private opts: ConfirmOptions;
+	connection: Connection;
+	env: DriftEnv;
 
-	public constructor(
-		programId: PublicKey,
-		connection: Connection,
-		wallet: Wallet,
-		opts: ConfirmOptions = Provider.defaultOptions()
-	) {
-		this.programId = programId;
+	public constructor(connection: Connection, env: DriftEnv) {
 		this.connection = connection;
-		this.wallet = wallet;
-		this.opts = opts;
+		this.env = env;
 	}
 
-	public async getPriceData(pricePublicKey: PublicKey): Promise<> {
+	public async getPrice(pricePublicKey: PublicKey): Promise<BN> {
 		const aggregatorAccount: AggregatorAccount = new AggregatorAccount({
 			program: await this.getProgram(),
 			publicKey: pricePublicKey,
 		});
 
-		console.log(await aggregatorAccount.loadData());
+		const aggregatorData = await aggregatorAccount.loadData();
+		return convertSwitchboardDecimal(
+			aggregatorData.latestConfirmedRound.result as SwitchboardDecimal
+		);
 	}
 
 	private program: Program;
@@ -41,17 +37,14 @@ export class SwitchboardClient {
 			return this.program;
 		}
 
-		const provider = new Provider(this.connection, this.wallet, this.opts);
-		const idl = await Program.fetchIdl(SBV2_MAINNET_PID, provider);
-
-		if (!idl) {
-			throw new Error(`failed to read idl for ${this.programId}`);
-		}
-
-		return new Program(idl, this.programId, provider);
+		this.program = loadSwitchboardProgram(this.env, this.connection);
+		return this.program;
 	}
 }
 
-export function getSwitchboardProgramId(env: DriftEnv): PublicKey {
-	return env === 'mainnet-beta' ? SBV2_MAINNET_PID : SBV2_DEVNET_PID;
+function convertSwitchboardDecimal(switchboardDecimal: SwitchboardDecimal): BN {
+	const switchboardPrecision = TEN.pow(new BN(switchboardDecimal.scale));
+	return switchboardDecimal.mantissa
+		.mul(MARK_PRICE_PRECISION)
+		.div(switchboardPrecision);
 }
