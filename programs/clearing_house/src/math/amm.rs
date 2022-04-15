d6a09dce41d1aa15165063061b7e74df5e855254
Chris Heaney <chrisheaney30@gmail.com>
2022-04-15 14:01:29 -0500

    more precision for reserves

--- programs/clearing_house/src/math/amm.rs
+++ programs/clearing_house/src/math/amm.rs
@@ -343,25 +343,30 @@ fn calculate_spread_reserve(
 ) -> ClearingHouseResult<u128> {
     let spread_reserve_scale = match asset_type {
         AssetType::Base => BID_ASK_SPREAD_PRECISION
-            .checked_mul(10_000_000_u128) // 2^8
+            .checked_mul(1_000_000_000_000_u128) // 2^8
             .ok_or_else(math_error!())?
             .checked_div(spread_ratio)
             .ok_or_else(math_error!())?
             .nth_root(2),
         AssetType::Quote => spread_ratio
-            .checked_mul(10_000_000_u128) // 2^8
+            .checked_mul(1_000_000_000_000_u128) // 2^8
             .ok_or_else(math_error!())?
             .checked_div(BID_ASK_SPREAD_PRECISION)
             .ok_or_else(math_error!())?
             .nth_root(2),
     };
 
+    msg!("reserve {}", reserve);
+    msg!("spread_reserve_scale {}", spread_reserve_scale);
+
     // f (fraction of reserves to achieve target price)
     let spread_reserve = reserve
         .checked_mul(spread_reserve_scale)
         .ok_or_else(math_error!())?
-        .checked_div(10_000_u128) // 2^8
+        .checked_div(1_000_000_u128) // 2^8
         .ok_or_else(math_error!())?;
+
+    msg!("spread_reserve {}", spread_reserve);
 
     Ok(spread_reserve)
 }
