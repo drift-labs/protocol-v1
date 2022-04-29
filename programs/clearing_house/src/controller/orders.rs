2c9ac17458a89184c42a5f5bcdb972ddf620a949
Chris Heaney <chrisheaney30@gmail.com>
2022-04-29 08:35:37 -0700

    Revert "dont subtract rebate from total fees"
    This reverts commit 041a97d42fa87134dfbdd21dab33ff17b20dda93.

--- programs/clearing_house/src/controller/orders.rs
+++ programs/clearing_house/src/controller/orders.rs
@@ -693,12 +693,10 @@ pub fn fill_order(
     };
 
     // Increment the user's total fee variables
-    if user_fee > 0 {
-        user.total_fee_paid = user
-            .total_fee_paid
-            .checked_add(user_fee.unsigned_abs())
-            .ok_or_else(math_error!())?;
-    }
+    user.total_fee_paid = user
+        .total_fee_paid
+        .checked_add(user_fee)
+        .ok_or_else(math_error!())?;
     user.total_token_discount = user
         .total_token_discount
         .checked_add(token_discount)
